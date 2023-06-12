4164eb08264d938b1fb350f4798b9fa0020b5c94
ChaksW <runsheng.wu@outlook.com>
2023-06-12 08:26:18 +0800

    upload code

--- PROJECT/SCGA/scData/apps.py
+++ PROJECT/SCGA/scData/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SourcecoveragedataConfig(AppConfig):
+class ScdataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sourceCoverageData'
+    name = 'scData'
