86f6bc9bed2b18a307c5b02e6adf909a2478ba1f
ChaksW <runsheng.wu@outlook.com>
2023-06-15 14:59:18 +0800

    upload code

--- PROJECT/SCGA/scData/models.py
+++ PROJECT/SCGA/scData/models.py
@@ -46,73 +46,13 @@ class Load(models.Model):
         return f"{self.load} <- {self.Func}"
 # A Date group of SCGA result of a specific load <- Functionality <- Certification <- Project
 
-# Test Plan of a specific Process <- Load
+# Test Plan and Test Expection (A|B|C)of a specific Process <- Load
 
 
-class TestPlan(models.Model):
-    # Process like (GgfPfd Mws...)
-    process = models.CharField(verbose_name="Process", max_length=50)
-    # File Name(x.cpp)
-    fName = models.CharField(verbose_name="File Name", max_length=50)
-    # Function in file
-    func = models.CharField(verbose_name="Function", max_length=50)
-    # Load Number
-    swLoad = models.ForeignKey(
-        'Load', on_delete=models.CASCADE, db_column='Load')
-    # Analyst
-    analyst = models.CharField(verbose_name="Analyst", max_length=50)
-    # Work Site of Analyst
-    site = models.CharField(verbose_name="Site", max_length=50)
-    # Date of Test
-    startDate = models.DateTimeField(verbose_name="Start Date")
-    # Data field that recored the precentage of coverage of test
-    PRECENTAGE_VALIDATOR = [MinValueValidator(0.0), MaxValueValidator(100.0)]
-    mcdcCoverage = models.DecimalField(
-        verbose_name="MC/DC Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
-    eventCoverage = models.DecimalField(
-        verbose_name="Event Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
-    analysisCoverage = models.DecimalField(
-        verbose_name="Analysis Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
-    totalScCoverage = models.DecimalField(
-        verbose_name="Total SC Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
-    # execution Statue
-    branches = models.IntegerField(verbose_name="Branches")
-    events = models.IntegerField(verbose_name="Events")
-    note = models.CharField(verbose_name="Note", max_length=50)
+# Each row of Test Plan a coverage of MC/DC and Event of a specific function
+# for those functions that are not covered according to SCGA, we can use Test Exception to record the reason
 
-    # Module Structure Data (Branches and Entries/Exits)
-    # Covered Decisions
-    coveredDecisions = models.IntegerField(verbose_name="Decisions")
-    # Covered Conditions
-    coveredConditons = models.IntegerField(verbose_name="Conditions")
-    # Covered Cases
-    coveredCases = models.IntegerField(verbose_name="Cases")
-    # Covered Events
-    coveredEvents = models.IntegerField(verbose_name="Events")
-    # Total Decisions
-    totalDecisions = models.IntegerField(verbose_name="Decisions")
-    # Total Conditions
-    totalConditions = models.IntegerField(verbose_name="Conditions")
-    # Total Cases
-    totalCases = models.IntegerField(verbose_name="Cases")
-    # Total Events
-    totalEvents = models.IntegerField(verbose_name="Events")
-
-    # OverSight
-    YorN = (
-        ('Y', 'Yes'),
-        ('N', 'No'),
-        (' ', ' '),
-    )
-    overSight = models.CharField(max_length=1, choices=YorN, default=' ')
-
-    # Defect Classification
-    tech = models.CharField(verbose_name="Tech", max_length=50)
-    nonTech = models.CharField(verbose_name="Non-Tech", max_length=50)
-    processDefect = models.CharField(
-        verbose_name="Process Defect", max_length=50)
-
-
+# Test Exception recored the reason why a specific function is not covered according to SCGA
 class TestException(models.Model):
     # create fields with data named as: note_, module_, function_ line_ reqTag_ analyst_ class_
     # analysisSummary_, correctiveAction_, issue_ with 'Y/N' choices, applicable_ with 'PAR/CR'
@@ -154,3 +94,80 @@ class TestException(models.Model):
     )
     applicable = models.CharField(
         verbose_name="Applicable", max_length=10, choices=PAR_OR_CR, default=' ')
+
+
+class TestPlan(models.Model):
+    # create choices with A|B|C
+    LEVEL_OPT = {
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+    }
+    level = models.CharField(verbose_name="Level",
+                             max_length=50, choices=LEVEL_OPT, default='', null=True, blank=True)
+    # Process like (GgfPfd Mws...)
+    process = models.CharField(verbose_name="Process", max_length=50)
+    # File Name(x.cpp)
+    fName = models.CharField(verbose_name="File Name", max_length=50)
+    # Function in file
+    func = models.CharField(verbose_name="Function", max_length=50)
+    # Use this to replace the note
+    # funcID = models.IntegerField(unique=True, primary_key=True)
+    # Load Number
+    swLoad = models.ForeignKey(
+        'Load', on_delete=models.CASCADE, db_column='Load')
+    # Analyst
+    analyst = models.CharField(verbose_name="Analyst", max_length=50)
+    # Work Site of Analyst
+    site = models.CharField(verbose_name="Site", max_length=50)
+    # Date of Test
+    startDate = models.DateTimeField(verbose_name="Start Date")
+    # Data field that recored the precentage of coverage of test
+    PRECENTAGE_VALIDATOR = [MinValueValidator(0.0), MaxValueValidator(100.0)]
+    mcdcCoverage = models.DecimalField(
+        verbose_name="MC/DC Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
+    eventCoverage = models.DecimalField(
+        verbose_name="Event Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
+    analysisCoverage = models.DecimalField(
+        verbose_name="Analysis Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
+    totalScCoverage = models.DecimalField(
+        verbose_name="Total SC Coverage", max_digits=4, decimal_places=2, validators=PRECENTAGE_VALIDATOR)
+    # execution Statue
+    branches = models.IntegerField(verbose_name="Branches")
+    events = models.IntegerField(verbose_name="Events")
+    note = models.CharField(verbose_name="Note", max_length=50)
+    testException = models.OneToOneField(
+        TestException, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Test Exception")
+    # note = models.OneToOneField(TestException, on_delete=models.CASCADE)
+
+    # Module Structure Data (Branches and Entries/Exits)
+    # Covered Decisions
+    coveredDecisions = models.IntegerField(verbose_name="Decisions")
+    # Covered Conditions
+    coveredConditons = models.IntegerField(verbose_name="Conditions")
+    # Covered Cases
+    coveredCases = models.IntegerField(verbose_name="Cases")
+    # Covered Events
+    coveredEvents = models.IntegerField(verbose_name="Events")
+    # Total Decisions
+    totalDecisions = models.IntegerField(verbose_name="Decisions")
+    # Total Conditions
+    totalConditions = models.IntegerField(verbose_name="Conditions")
+    # Total Cases
+    totalCases = models.IntegerField(verbose_name="Cases")
+    # Total Events
+    totalEvents = models.IntegerField(verbose_name="Events")
+
+    # OverSight
+    YorN = (
+        ('Y', 'Yes'),
+        ('N', 'No'),
+        (' ', ' '),
+    )
+    overSight = models.CharField(max_length=1, choices=YorN, default=' ')
+
+    # Defect Classification
+    tech = models.CharField(verbose_name="Tech", max_length=50)
+    nonTech = models.CharField(verbose_name="Non-Tech", max_length=50)
+    processDefect = models.CharField(
+        verbose_name="Process Defect", max_length=50)
