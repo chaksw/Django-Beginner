0e88b99e4731902cbe102b5857fdb20fa976b4d1
chaksw <runsheng.wu@outlook.com>
2023-06-25 23:41:15 +0800

    update code

--- PROJECT/SCGA_Project/SCGA/scData/admin.py
+++ PROJECT/SCGA_Project/SCGA/scData/admin.py
@@ -14,13 +14,15 @@ admin.site.site_header = 'SCGA Admin Panel'
 # class ImportAdmin(DjangoObjectActions, admin.ModelAdmin):
 #     def imports(modeladmin, request, queryset):
 #         modeladmin.message_user(
-#             request, 
+#             request,
 #             ngettext(
 #                 '%d record was successfully imported.',
 #                 '%d records were successfully imported.',
 #             )
 #         )
 #     changelist_actions = ('imports',)
+
+
 class MyModelModelAdmin(ExtraButtonsMixin, admin.ModelAdmin):
 
     @button(permission='demo.add_demomodel1',
@@ -30,17 +32,17 @@ class MyModelModelAdmin(ExtraButtonsMixin, admin.ModelAdmin):
         self.message_user(request, 'refresh called')
         # Optional: returns HttpResponse
         return HttpResponseRedirectToReferrer(request)
-    
+
     @button(html_attrs={'style': 'background-color:#DC6C6C;color:black'})
     def confirm(self, request):
         def _action(request):
             pass
 
         return confirm_action(self, request, _action, "Confirm action",
-                          "Successfully executed", )
+                              "Successfully executed", )
 
-    @link(href=None, 
-          change_list=False, 
+    @link(href=None,
+          change_list=False,
           html_attrs={'target': '_new', 'style': 'background-color:var(--button-bg)'})
     def search_on_google(self, button):
         original = button.context['original']
@@ -62,33 +64,39 @@ class MyModelModelAdmin(ExtraButtonsMixin, admin.ModelAdmin):
         return HttpResponse("GET")
 
 
-
 class FunctionalityInline(admin.TabularInline):
     model = Functionality
-    
+
+
 class LoadInline(admin.TabularInline):
     model = Load
-    
+
+
 class TestPlanInline(admin.TabularInline):
     # The name of a DateField or DateTimeField in your model
     # date_hierarchy = 'imported_date'
     model = TestPlan
-    
+
+
 class TestExceptionInline(admin.TabularInline):
     model = TestException
 
 
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     model = Project
     inlines = [FunctionalityInline]
 
 
+@admin.register(Functionality)
 class FunctionalityAdmin(admin.ModelAdmin):
     model = Functionality
     inlines = [LoadInline]
     list_display = ('func', 'Project')
-    fields = ('func', 'Project')    
+    fields = ('func', 'Project')
 
+
+@admin.register(Load)
 class LoadAdmin(admin.ModelAdmin):
     model = Load
     inlines = [TestPlanInline]
@@ -96,52 +104,54 @@ class LoadAdmin(admin.ModelAdmin):
     fields = ('load', 'Func')
 
 
+@admin.register(TestPlan)
 class TestPlanAdmin(admin.ModelAdmin):
     model = TestPlan
     inlines = [TestExceptionInline]
     empty_value_display = '-empty-'  # The string to display in lieu of an empty value
     # a list_display tuple of field names to display, as columns, on the change list page for the object
-    list_display = ('func','level', 'process', 'fName', 
+    list_display = ('func', 'level', 'process', 'fName',
                     'swLoad', 'analyst', 'site', 'startDate')
     # a list_filter tuple of field names that will be displayed in the right sidebar of the change list page of the admin
-    list_filter = ('func','level', 'process', 'fName', 
-                    'swLoad', 'analyst', 'site', 'startDate')
+    list_filter = ('func', 'level', 'process', 'fName',
+                   'swLoad', 'analyst', 'site', 'startDate')
     # a search_fields tuple of field names that will be searched whenever somebody submits a search query in that text box
-    search_fields = ('func','level', 'process', 'fName', 
-                    'swLoad', 'analyst', 'site', 'startDate')
+    search_fields = ('func', 'level', 'process', 'fName',
+                     'swLoad', 'analyst', 'site', 'startDate')
     fieldsets = [
-        ('Test Plan', {'fields': ['func','level', 'process', 'fName',
-                            'swLoad', 'analyst', 'site', 'startDate']}),
+        ('Test Plan', {'fields': ['func', 'level', 'process', 'fName',
+                                  'swLoad', 'analyst', 'site', 'startDate']}),
         ("Coverage Result", {
-            "classes":["collapse"],
+            "classes": ["collapse"],
             "fields": ["mcdcCoverage", "analysisCoverage", "totalScCoverage", "coveredBranches", "coveredPairs", "coveredStatements", "totalBranches", "totalPairs", "totalStatements",
-                       "overSight", "tech", "nonTech", "processDefect"]},)      
+                       "overSight", "tech", "nonTech", "processDefect"]},)
     ]
-    
-    
+
+
+@admin.register(TestException)
 class TestExceptionAdmin(admin.ModelAdmin):
     model = TestException
     empty_value_display = '-empty-'  # The string to display in lieu of an empty value
     list_display = ('id', 'module', 'function', 'SWline', 'InsSWline',
                     'reqTag', 'analyst', 'ucClassification', 'testPlan')
     list_filter = ('id', 'module', 'function', 'SWline', 'InsSWline',
-                    'reqTag', 'analyst', 'ucClassification', 'testPlan')
+                   'reqTag', 'analyst', 'ucClassification', 'testPlan')
     search_fields = ('id', 'module', 'function', 'SWline', 'InsSWline',
-                    'reqTag', 'analyst', 'ucClassification', 'testPlan')
+                     'reqTag', 'analyst', 'ucClassification', 'testPlan')
     fieldsets = [
-        ('Test Exception', {'fields': ['module', 'function', 'SWline', 'InsSWline', 'reqTag', 'analyst', 'ucClassification', 'testPlan']}),
+        ('Test Exception', {'fields': ['module', 'function', 'SWline',
+         'InsSWline', 'reqTag', 'analyst', 'ucClassification', 'testPlan']}),
         ("Analysis Fields", {
             "classes": ["collapse"],
             "fields": ["analysisSummary", "correctiveAction", "issue", "applicable"]},),
     ]
     list_display_links = ["function"]
-    
-    
+
 
 # register all models imported from models using admin.site.register()
-admin.site.register(Project, ProjectAdmin)
-# admin.site.register(Certification)
-admin.site.register(Functionality, FunctionalityAdmin)
-admin.site.register(Load, LoadAdmin)
-admin.site.register(TestPlan, TestPlanAdmin)
-admin.site.register(TestException, TestExceptionAdmin)
+# admin.site.register(Project, ProjectAdmin)
+# # admin.site.register(Certification)
+# admin.site.register(Functionality, FunctionalityAdmin)
+# admin.site.register(Load, LoadAdmin)
+# admin.site.register(TestPlan, TestPlanAdmin)
+# admin.site.register(TestException, TestExceptionAdmin)
