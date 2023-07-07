2d63abc082b5ef62d875fd2d69e9797bc262b3d1
chaksw <runsheng.wu@outlook.com>
2023-07-07 23:37:23 +0800

    update code

--- PROJECT/SCGA_Project/SCGA/static/base_func.js
+++
@@ -1,6 +0,0 @@
-const importModal = document.getElementById("importModal");
-const importBtn = document.getElementById("importBtn");
-
-importModal.addEventListener("show.bs.modal", () => {
-    importBtn.focus();
-});
--- PROJECT/test/base_func.js
+++
@@ -1,8 +0,0 @@
-const importBtn = document.getElementById("importBtn");
-const importModal = new bootstrap.Modal(document.getElementById("importModal"));
-
-const showModal = function () {
-    importModal.show();
-};
-
-importBtn.addEventListener("click", showModal);
