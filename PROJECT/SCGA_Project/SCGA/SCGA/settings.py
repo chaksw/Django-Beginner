15dfdd9915bd698016d6455fa542b85fe3b74235
chaksw <runsheng.wu@outlook.com>
2023-07-12 23:12:31 +0800

    update code

--- PROJECT/SCGA_Project/SCGA/SCGA/settings.py
+++ PROJECT/SCGA_Project/SCGA/SCGA/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SCGA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates/')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
